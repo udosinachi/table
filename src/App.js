d236e2dfba99dce1df180234faf6d59b04213ac5
udosinachi <udosipat@gmail.com>
2020-09-23 17:17:26 +0100

    Home3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './home/Home'
 import Home2 from './home2/Home2'
+import Home3 from './home3/Home3'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <nav style={{background: '#252079', width: '100%'}}>Nav</nav><br/>
         <Home />
         <Home2 />
+        <Home3 />
     </div>
   );
 }
---
+++ src/home3/Home3.js
@@ -0,0 +1,72 @@
+import React from 'react'
+
+function Home3(){
+    return(
+        <div>
+            <section class="hiw-area text-center section-bg padding-top-100px padding-bottom-85px">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="section-heading">
+                    <h2 class="sec__title">How Zobstar Works for You?</h2>
+                    <p class="sec__desc">
+                        Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit <br/>
+                        facere possimus, omnis voluptas assumenda est, omnis
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-lg-4 column-td-6">
+                <div class="icon-box">
+                    <div class="icon-element">
+                        <i class="la la-search-plus"></i>
+                        <span class="info-number">1</span>
+                    </div>
+                    <div class="info-content mt-4">
+                       <h4 class="info__title mb-3">Find the Right Job</h4>
+                        <p class="info__desc">
+                            Sed quia lipsum dolor sit atur adipiscing elit is nunc quis
+                            tellus sed ligula porta ultricies quis nec nec magna
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 column-td-6">
+                <div class="icon-box">
+                    <div class="icon-element">
+                        <i class="las la-comment-dollar"></i>
+                        <span class="info-number">2</span>
+                    </div>
+                    <div class="info-content mt-4">
+                        <h4 class="info__title mb-3">Research Companies</h4>
+                        <p class="info__desc">
+                            Sed quia lipsum dolor sit atur adipiscing elit is nunc quis
+                            tellus sed ligula porta ultricies quis nec nec magna
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 column-td-6">
+                <div class="icon-box">
+                    <div class="icon-element">
+                        <i class="la la-rocket"></i>
+                        <span class="info-number">3</span>
+                    </div>
+                    <div class="info-content mt-4">
+                        <h4 class="info__title mb-3">Save &amp; Apply</h4>
+                        <p class="info__desc">
+                            Sed quia lipsum dolor sit atur adipiscing elit is nunc quis
+                            tellus sed ligula porta ultricies quis nec nec magna
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+        </div>
+    )
+}
+
+export default Home3
